1b95e5b8941618ad4eaeff4ac2884e091a1166df
FatimaAtiyya <57375494+fati-ma@users.noreply.github.com>
2020-12-26 03:56:17 +0200

    Add files via upload

--- train.py
+++ train.py
@@ -18,15 +18,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 ds = TabularDatasetFactory.from_delimited_files(path='https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')
  
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-x_train, x_test, y_train, y_test= train_test_split(x,y)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -52,7 +43,17 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
+    return x_df, y_df
+
+x, y = clean_data(ds)
+
+run = Run.get_context()
+
+# TODO: Split data into train and test sets.
+### YOUR CODE HERE ###
+
+x_train, x_test, y_train, y_test= train_test_split(x,y)
 
 def main():
     # Add arguments to script
@@ -70,6 +71,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
