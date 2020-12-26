27dc52610f3bb10156768331cd5d35675ea72276
FatimaAtiyya <57375494+fati-ma@users.noreply.github.com>
2020-12-27 00:39:01 +0200

    train.py

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
 
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
